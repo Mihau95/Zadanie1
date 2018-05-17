f84e9b6a1371e19197fdd796269336802f89c87c
Mihau95 <misiek0019@wp.pl>
2018-05-17 17:57:48 +0200

    Druga wersja

--- Zadanie1.java
+++ Zadanie1.java
@@ -3,6 +3,8 @@ class Zadanie1 {
   System.out.println("Ucze");
   System.out.println("sie");
   System.out.println("programowania");
+  System.out.println("i");
+  System.out.println("commitowania");
 
  }
 }
